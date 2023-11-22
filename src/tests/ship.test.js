47db5bffda82ecdcf9aceac9bad620121429309e
Cory Chilton <coryhookchilton@gmail.com>
2023-11-21 20:30:45 -0800

    Add ship hit and isSunk functionality with tests

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,6 +1,47 @@
 import Ship from '../modules/ship';
 
-test('Constructing ship', () => {
+test('Ship instantiation', () => {
   const ship1 = new Ship(5);
   expect(ship1.length).toBe(5);
+  expect(ship1.hits).toBe(0);
+});
+
+test('Ship hit once', () => {
+  const ship2 = new Ship(3);
+  ship2.hit();
+  expect(ship2.hits).toBe(1);
+});
+
+test('Ship hit multiple times', () => {
+  const ship3 = new Ship(3);
+  ship3.hit();
+  ship3.hit();
+  ship3.hit();
+  expect(ship3.hits).toBe(3);
+});
+
+test('isSunk with no hits', () => {
+  const ship4 = new Ship(4);
+  expect(ship4.isSunk()).toBe(false);
+});
+
+test('isSunk with too few hits', () => {
+  const ship5 = new Ship(2);
+  ship5.hit();
+  expect(ship5.isSunk()).toBe(false);
+});
+
+test('isSunk with exact number of hits', () => {
+  const ship5 = new Ship(2);
+  ship5.hit();
+  ship5.hit();
+  expect(ship5.isSunk()).toBe(true);
+});
+
+test('isSunk with more hits than necessary', () => {
+  const ship5 = new Ship(2);
+  ship5.hit();
+  ship5.hit();
+  ship5.hit();
+  expect(ship5.isSunk()).toBe(true);
 });
