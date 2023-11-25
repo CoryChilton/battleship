95475ec35b51ea139cda2043eb1e24557156cde5
Cory Chilton <coryhookchilton@gmail.com>
2023-11-25 15:03:56 -0800

    Initial gameboard commit

---
+++ src/modules/gameboard.js
@@ -0,0 +1,43 @@
+import Ship from './ship';
+
+export default class Gameboard {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+    this.ships = [];
+    this.hits = new Set();
+    this.misses = new Set();
+    this.totalShipLength = 0;
+  }
+
+  placeShip(xStart, yStart, xEnd, yEnd) {
+    if (
+      xStart < 0 ||
+      yStart < 0 ||
+      xEnd > this.width - 1 ||
+      yEnd > this.height - 1
+    ) {
+      throw new RangeError('Coordinates out of bounds.');
+    }
+    this.ships.push(new Ship(xStart, yStart, xEnd, yEnd));
+    this.totalShipLength += this.ships[this.ships.length - 1].length;
+  }
+
+  receiveAttack(x, y) {
+    if (x < 0 || x >= this.width || y < 0 || y >= this.height) {
+      throw new RangeError('Coordinates out of bounds.');
+    }
+    for (const ship of this.ships) {
+      if (ship.isOnShip(x, y)) {
+        ship.hit();
+        this.hits.add(`${x},${y}`);
+        return;
+      }
+    }
+    this.misses.add(`${x},${y}`);
+  }
+
+  allSunk() {
+    return this.hits.size === this.totalShipLength;
+  }
+}
---
+++ src/tests/gameboard.test.js
@@ -0,0 +1,73 @@
+import Gameboard from '../modules/gameboard';
+
+test('Place ship normally', () => {
+  const gameboard1 = new Gameboard(5, 5);
+  gameboard1.placeShip(0, 0, 3, 0);
+  expect(gameboard1.ships[0].xStart).toBe(0);
+  expect(gameboard1.ships[0].yStart).toBe(0);
+  expect(gameboard1.ships[0].xEnd).toBe(3);
+  expect(gameboard1.ships[0].yEnd).toBe(0);
+  expect(gameboard1.ships[0].length).toBe(4);
+  gameboard1.placeShip(0, 1, 4, 1);
+  expect(gameboard1.totalShipLength).toBe(9);
+});
+
+test('Place ship out of bounds', () => {
+  const gameboard1 = new Gameboard(5, 5);
+  expect(() => {
+    gameboard1.placeShip(0, 0, 5, 0);
+  }).toThrow();
+});
+
+test('receiveAttack hits', () => {
+  const gameboard1 = new Gameboard(5, 5);
+  gameboard1.placeShip(0, 0, 3, 0);
+  gameboard1.receiveAttack(0, 0);
+  expect(gameboard1.hits.has('0,0')).toBe(true);
+});
+
+test('receiveAttack misses', () => {
+  const gameboard1 = new Gameboard(5, 5);
+  gameboard1.placeShip(0, 0, 3, 0);
+  gameboard1.receiveAttack(0, 1);
+  expect(gameboard1.misses.has('0,1')).toBe(true);
+});
+
+test('receiveAttack out of bounts', () => {
+  const gameboard1 = new Gameboard(5, 5);
+  gameboard1.placeShip(0, 0, 3, 0);
+  expect(() => {
+    gameboard1.receiveAttack(0, 5);
+  }).toThrow();
+});
+
+test('allSunk with too few hits', () => {
+  const gameboard1 = new Gameboard(5, 5);
+  gameboard1.placeShip(0, 0, 3, 0);
+  gameboard1.receiveAttack(0, 0);
+  expect(gameboard1.allSunk()).toBe(false);
+});
+
+test('allSunk with correct amount of hits', () => {
+  const gameboard1 = new Gameboard(5, 5);
+  gameboard1.placeShip(0, 0, 1, 0);
+  gameboard1.receiveAttack(0, 0);
+  gameboard1.receiveAttack(1, 0);
+  expect(gameboard1.allSunk()).toBe(true);
+});
+
+test('allSunk with too few hits and misses', () => {
+  const gameboard1 = new Gameboard(5, 5);
+  gameboard1.placeShip(0, 0, 1, 0);
+  gameboard1.receiveAttack(0, 0);
+  gameboard1.receiveAttack(0, 1);
+  expect(gameboard1.allSunk()).toBe(false);
+});
+
+test('allSunk with multiple hits to the same place', () => {
+  const gameboard1 = new Gameboard(5, 5);
+  gameboard1.placeShip(0, 0, 1, 0);
+  gameboard1.receiveAttack(0, 0);
+  gameboard1.receiveAttack(0, 0);
+  expect(gameboard1.allSunk()).toBe(false);
+});
