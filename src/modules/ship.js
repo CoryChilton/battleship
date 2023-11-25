0cf97743737a01b3d96dfca85c7e54119f65ce5e
Cory Chilton <coryhookchilton@gmail.com>
2023-11-24 17:22:02 -0800

    Change ship implementation to use coordinates

--- src/modules/ship.js
+++ src/modules/ship.js
@@ -1,6 +1,13 @@
 export default class Ship {
-  constructor(length) {
-    this.length = length;
+  constructor(xStart, yStart, xEnd, yEnd) {
+    if (xStart !== xEnd && yStart !== yEnd) {
+      throw new RangeError('Invalid coordinates');
+    }
+    this.length = Math.abs(xStart - xEnd) || Math.abs(yStart - yEnd);
+    this.xStart = xStart;
+    this.yStart = yStart;
+    this.xEnd = xEnd;
+    this.yEnd = yEnd;
     this.hits = 0;
   }
 
--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,13 +1,25 @@
 import Ship from '../modules/ship';
 
-test('Ship instantiation', () => {
-  const ship1 = new Ship(5);
+test('Ship instantiation horizontal', () => {
+  const ship1 = new Ship(0, 0, 5, 0);
   expect(ship1.length).toBe(5);
   expect(ship1.hits).toBe(0);
 });
 
+test('Ship instantiation vertical', () => {
+  const ship1 = new Ship(0, 0, 0, 5);
+  expect(ship1.length).toBe(5);
+  expect(ship1.hits).toBe(0);
+});
+
+test('Diagonal ship', () => {
+  expect(() => {
+    new Ship(1, 1, 2, 2);
+  }).toThrow();
+});
+
 test('Ship hit once', () => {
-  const ship2 = new Ship(3);
+  const ship2 = new Ship(0, 0, 5, 0);
   ship2.hit();
   expect(ship2.hits).toBe(1);
 });
@@ -21,25 +33,25 @@ test('Ship hit multiple times', () => {
 });
 
 test('isSunk with no hits', () => {
-  const ship4 = new Ship(4);
+  const ship4 = new Ship(0, 0, 5, 0);
   expect(ship4.isSunk()).toBe(false);
 });
 
 test('isSunk with too few hits', () => {
-  const ship5 = new Ship(2);
+  const ship5 = new Ship(0, 0, 5, 0);
   ship5.hit();
   expect(ship5.isSunk()).toBe(false);
 });
 
 test('isSunk with exact number of hits', () => {
-  const ship5 = new Ship(2);
+  const ship5 = new Ship(0, 0, 2, 0);
   ship5.hit();
   ship5.hit();
   expect(ship5.isSunk()).toBe(true);
 });
 
 test('isSunk with more hits than necessary', () => {
-  const ship5 = new Ship(2);
+  const ship5 = new Ship(0, 0, 2, 0);
   ship5.hit();
   ship5.hit();
   ship5.hit();
